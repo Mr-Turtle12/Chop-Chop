2f0041c7319f706f64d388cbec8e16dd1e302472
DeanG <dmag2@kent.ac.uk>
2023-10-03 11:37:54 +0100

    Sudo code created

---
+++ Backend/Controller/Controller.py
@@ -0,0 +1,19 @@
+#Controls the flow of the whole of the back-end
+
+import json
+
+def test_print():
+    print("Hello")
+
+
+# Get the required recipe from the JSON files
+# Begin incrementing the steps.
+def start_recipe(recipe_name):
+    print("Recipe Started")
+
+def get_recipe_from_JSON(recipe_name):
+
+    #GET FROM JSON
+
+    # Save to JSON object
+
---
+++ Backend/main.py
@@ -0,0 +1,9 @@
+# This file is the entry point for the back end
+from Controller import Controller
+
+
+def main():
+    #Call start up in controller.
+    Controller.test_print()
+
+main()
