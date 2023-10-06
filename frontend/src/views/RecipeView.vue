a4b9c1d0a3c75e993181119a81bc6a4f03b033d1
Connor Wood <cw668@kent.ac.uk>
2023-10-06 13:16:26 +0100

    add `root.css\ file and `RecipeView.vue`

--- frontend/src/views/RecipeView.vue
+++ frontend/src/views/RecipeView.vue
@@ -1,6 +1,17 @@
 <template>
-  <a href="/">home view</a>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <nav>
+    <img> Back Button
+  </nav>
+  <div>
+    <p
+      v-for="x in y"
+      :key="x"
+    >
+      {{ x }}
+    </p>
   </div>
 </template>
+
+<script setup>
+const y = ['step1', 'step2', 'step3', 'step4']
+</script>
