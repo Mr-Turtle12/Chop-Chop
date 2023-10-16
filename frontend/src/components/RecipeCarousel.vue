acaad0ed98da9e61136ca53178557e9c3b09081a
Bethany Hill <berh2@kent.ac.uk>
2023-10-16 15:47:08 +0100

    basic step increment

---
+++ frontend/src/components/RecipeCarousel.vue
@@ -0,0 +1,27 @@
+<template>
+  <p
+    v-for="step in hello"
+    :key="step"
+  >
+    {{ step }}
+  </p>
+</template>
+
+<script setup>
+var index = 0
+const steps = ['step1','step2', 'step3', 'step4']
+var hello = function show(index) {
+    if (index > 0 && index < length(steps)) {
+        return [steps[index - 1], steps[index], steps[index+1]]   
+    }
+    if (index == 0) {
+        return [steps[index], steps[index+1]]   
+    }
+    else {
+        return [steps[index - 1], steps[index]]
+    }
+}
+</script>
+
+<style>
+</style>
