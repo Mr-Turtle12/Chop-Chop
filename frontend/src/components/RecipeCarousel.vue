fd679ce9c70cc05ae75d89384e03a7c3e1d6a46b
Bethany Hill <berh2@kent.ac.uk>
2023-10-17 13:42:56 +0100

    Computed Properties

--- frontend/src/components/RecipeCarousel.vue
+++ frontend/src/components/RecipeCarousel.vue
@@ -4,7 +4,7 @@
       <button
         class="c-recipe-carousel__button c-recipe-carousel__button--up"
         @click="
-          increment(currentIndex)"
+          increment(currentIndex + 1)"
       >
         up
       </button>
@@ -12,7 +12,7 @@
       <button
         class="c-recipe-carousel__button c-recipe-carousel__button--down"
         @click="
-          decrement(currentIndex)"
+          decrement(currentIndex - 1)"
       >
         down
       </button>
@@ -21,7 +21,7 @@
         previous
       </p>
       <p class="c-recipe-carousel__current js-current">
-        current
+        {{ recipe.name }}
       </p>
       <p class="c-recipe-carousel__next js-next">
         next
@@ -31,6 +31,8 @@
 </template>
 
 <script setup>
+import { reactive, computed } from 'vue'
+
 var currentIndex = 0
 const steps = ['step1','step2', 'step3', 'step4']
 
@@ -38,49 +40,68 @@ const previous = document.getElementsByClassName('js-previous')
 const current = document.getElementsByClassName('js-current')
 const next = document.getElementsByClassName('js-next')
 
+const recipe = reactive({
+    name: 'Test Recipe',
+    steps: [
+        'Step 1',
+        'Step 2',
+        'Step 3',
+        'Step 4'
+    ]
+})
+
+// const currentStep = computed((index) => {
+//     index + 1
+//     return recipe.steps[index]
+// })
+
 function increment(index) {
-    console.log(index)
-    if (index == 0 && index < steps.length - 1) {  // if first item in array
-        previous[0].innerHTML = 'null'
-        current[0].innerHTML = steps[index]
-        next[0].innerHTML = steps[index+1]
+    const currentStep = computed((index) => {
+        index + 1
+        return recipe.steps[index]
+    })
+    //     console.log(index)
+    //     if (index == 0 && index < steps.length - 1) {  // if first item in array
+    //         previous[0].innerHTML = 'null'
+    //         current[0].innerHTML = steps[index]
+    //         next[0].innerHTML = steps[index+1]
 
-        currentIndex++
-    }
-    else if (index > 0 && index < steps.length - 1) {
-        previous[0].innerHTML = steps[index - 1]
-        current[0].innerHTML = steps[index]
-        next[0].innerHTML = steps[index+1]
+    //         // currentIndex++
+    //     }
+    //     else if (index > 0 && index < steps.length - 1) {
+    //         previous[0].innerHTML = steps[index - 1]
+    //         current[0].innerHTML = steps[index]
+    //         next[0].innerHTML = steps[index+1]
 
-        currentIndex++
-    }
-    else if(index <= steps.length - 1) {
-        previous[0].innerHTML = steps[index - 1]
-        current[0].innerHTML = steps[index]
-        next[0].innerHTML = 'null'
-    }
+//         // currentIndex++
+//     }
+//     else if(index <= steps.length - 1) {
+//         previous[0].innerHTML = steps[index - 1]
+//         current[0].innerHTML = steps[index]
+//         next[0].innerHTML = 'null'
+//     }
 }
 
 function decrement(index) {
-    console.log(index)
-    if (index > 0 && index < steps.length - 1) {
-        currentIndex--
+//     console.log(index)
+//     if (index > 0 && index < steps.length - 1) {
+//         // currentIndex--
 
-        previous[0].innerHTML = steps[index - 1]
-        current[0].innerHTML = steps[index]
-        next[0].innerHTML = steps[index + 1]
-    }
-    else if (index == 0 && index < steps.length - 1) {
-        previous[0].innerHTML = 'null'
-        current[0].innerHTML = steps[index]
-        next[0].innerHTML = steps[index + 1]
-    }
-    else if(index >= steps.length - 1) {
-        currentIndex--
-        previous[0].innerHTML = steps[index - 1]
-        current[0].innerHTML = steps[index]
-        next[0].innerHTML = 'null'
-    }
+//         previous[0].innerHTML = steps[index - 1]
+//         current[0].innerHTML = steps[index]
+//         next[0].innerHTML = steps[index + 1]
+//     }
+//     else if (index == 0 && index < steps.length - 1) {
+//         previous[0].innerHTML = 'null'
+//         current[0].innerHTML = steps[index]
+//         next[0].innerHTML = steps[index + 1]
+//     }
+//     else if(index >= steps.length - 1) {
+//         // currentIndex--
+//         previous[0].innerHTML = steps[index - 1]
+//         current[0].innerHTML = steps[index]
+//         next[0].innerHTML = 'null'
+//     }
 }
 </script>
 
