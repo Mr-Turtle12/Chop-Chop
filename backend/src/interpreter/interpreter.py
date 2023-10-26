a153fc0cf6cf8f64339db7f3fb5a72db9a197dd0
William <wah20@kent.ac.uk>
2023-10-26 16:11:34 +0100

    Change Interpreter to have loop and get the current step via queue and set an event

--- backend/src/interpreter/interpreter.py
+++ backend/src/interpreter/interpreter.py
@@ -5,22 +5,27 @@ import config
 class Interpreter:
     def __init__(self):
         # set up Cameras depends if it's a one or two camera set up
-        self.detect_prep = objectDetection.ObjectDetection(config.CAMERA_IDS[0])
+        self.prep_camera = objectDetection.ObjectDetection(config.CAMERA_IDS[0])
+        self.current_step = []
         if len(config.CAMERA_IDS) > 1:
-            self.detect_cook = objectDetection.ObjectDetection(config.CAMERA_IDS[1])
+            self.cook_camera = objectDetection.ObjectDetection(config.CAMERA_IDS[1])
 
-    # Pass into from json the camera name and the objects you want to check e.g [progressionObject , inhibitor]
-    def check_step(self, camera_name, object_array):
-        object_array.append("Hands")
-        if camera_name == "cook":
-            return self.detect_cook.check_items(object_array) == [
-                True,
-                False,
-                False,
-            ]
+    # This loop will do run in another thread and will get the current step passed into it via the in_q and set a event once the step has been detected
+    def detection_loop(self, in_q, event):
+        while True:
+            if in_q.empty():
+                self.current_step = in_q.get()
+                event.clear()
+            # logic for when it's str interrupt
+            if self.check_step():
+                event.set()
+
+    # Pass into from json the camera name and the objects you want to check e.g [camera, progressionObject , inhibitor]
+    def check_step(self):
+        progressionObject = self.current_step[1]
+        inhibitor = self.current_step[2]
+        inhibitor.append("hand")
+        if self.current_step[0] == "cook":
+            return self.cook_camera.check_items(progressionObject, inhibitor)
         else:
-            return self.detect_prep.check_items(object_array) == [
-                True,
-                False,
-                False,
-            ]
+            return self.prep_camera.check_items(progressionObject, inhibitor)
