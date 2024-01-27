185e4ffb1baefe82c5ee0bc9135e0144feb1bdd6
DeanG <dmag2@kent.ac.uk>
2024-01-27 12:34:40 +0000

    fixed issue

--- backend/src/api/websocket.py
+++ backend/src/api/websocket.py
@@ -52,7 +52,7 @@ async def consumer_handler(websocket):
                 await websocket.send("Started")
 
             # Sets current step (to be implemented later)
-            case ("step", step_number):
+            case ("set", step_number):
                 log(f">>> step set {step_number}", "API")
                 CONTROLLER_INSTANCE.set_step(step_number)
                 await websocket.send(f"set step {step_number}")
